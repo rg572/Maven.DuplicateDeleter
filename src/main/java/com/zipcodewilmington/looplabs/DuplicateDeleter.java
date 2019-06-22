8a06bb87a62126395f862ae2488389fb60431535
robertgordon <robertgordon.subscript2@gmail.com>
2019-06-21 23:03:06 -0400

    unabstracted the methods in DuplicateDeleter. All tests passing

--- src/main/java/com/zipcodewilmington/looplabs/DuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/DuplicateDeleter.java
@@ -10,10 +10,63 @@ public abstract class DuplicateDeleter<T> implements DuplicateDeleterInterface<T
         this.array = intArray;
     }
 
-    abstract public T[] removeDuplicates(int maxNumberOfDuplications);
+    public T[] removeDuplicates(int maxNumberOfDuplications){
+        boolean[] dontCopy = new boolean[array.length];
+        boolean[] beenChecked = new boolean[array.length];
+        Integer elementsRemoved = 0;
+
+        for(int i = 0; i < array.length; i++){
+            if(!beenChecked[i]){
+                Integer numberOfDuplicates = 1;
+                for(int j = i+1; j < array.length; j++){
+                    if(array[j].equals(array[i])){
+                        numberOfDuplicates++;
+                        beenChecked[j] = true;
+                    }
+                }
+                if(numberOfDuplicates >= maxNumberOfDuplications){
+                    elementsRemoved += numberOfDuplicates;
+                    for(int j = i; j < array.length; j++){
+                        if(array[j].equals(array[i])){
+                            dontCopy[j] = true;
+                        }
+                    }
+                }
+            }
+        }
+        return remover(elementsRemoved, dontCopy);
+    }
 
 
-    abstract public T[] removeDuplicatesExactly(int exactNumberOfDuplications);
+    public T[] removeDuplicatesExactly(int exactNumberOfDuplications){
+        boolean[] dontCopy = new boolean[array.length];
+        boolean[] beenChecked = new boolean[array.length];
+        Integer elementsRemoved = 0;
+
+        for(int i = 0; i < array.length; i++){
+            if(!beenChecked[i]){
+                Integer numberOfDuplicates = 1;
+                for(int j = i+1; j < array.length; j++){
+                    if(array[j].equals(array[i])){
+                        numberOfDuplicates++;
+                        beenChecked[j] = true;
+                    }
+                }
+                if(numberOfDuplicates == exactNumberOfDuplications){
+                    elementsRemoved += numberOfDuplicates;
+                    for(int j = i; j < array.length; j++){
+                        if(array[j].equals(array[i])){
+                            dontCopy[j] = true;
+                        }
+                    }
+                }
+            }
+        }
+        return remover(elementsRemoved, dontCopy);
+    }
+
+    abstract protected T[] remover(Integer elementsRemoved, boolean[] dontCopy);
+
 
 
 }
--- src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
@@ -9,13 +9,15 @@ public final class StringDuplicateDeleter extends DuplicateDeleter<String> {
         super(intArray);
     }
 
-    @Override
-    public String[] removeDuplicates(int maxNumberOfDuplications) {
-        return new String[0];
-    }
-
-    @Override
-    public String[] removeDuplicatesExactly(int exactNumberOfDuplications) {
-        return new String[0];
+    protected String[] remover(Integer elementsRemoved, boolean[] dontCopy){
+        String[] newArr = new String[array.length - elementsRemoved];
+        Integer index = 0;
+        for(int i = 0; i < array.length; i++){
+            if(!dontCopy[i]){
+                newArr[index] = array[i];
+                index++;
+            }
+        }
+        return newArr;
     }
 }
