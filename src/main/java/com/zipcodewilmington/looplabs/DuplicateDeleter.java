e2853e94ad883e91b778eb39b4e425eef97dcc01
robertgordon <robertgordon.subscript2@gmail.com>
2019-06-21 22:47:36 -0400

    IntegerDuplicateDeleter passing all tests

--- src/main/java/com/zipcodewilmington/looplabs/DuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/DuplicateDeleter.java
@@ -11,5 +11,9 @@ public abstract class DuplicateDeleter<T> implements DuplicateDeleterInterface<T
     }
 
     abstract public T[] removeDuplicates(int maxNumberOfDuplications);
+
+
     abstract public T[] removeDuplicatesExactly(int exactNumberOfDuplications);
+
+
 }
--- src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
@@ -5,4 +5,97 @@ package com.zipcodewilmington.looplabs;
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
+    public IntegerDuplicateDeleter(Integer[] intArray) {
+        super(intArray);
+    }
+
+    @Override
+    public Integer[] removeDuplicates(int maxNumberOfDuplications) {
+        boolean[] dontCopy = new boolean[array.length];
+        boolean[] beenChecked = new boolean[array.length];
+        Integer elementsRemoved = 0;
+
+        for(int i = 0; i < array.length; i++){
+            if(!beenChecked[i]){
+                Integer numberOfDuplicates = 1;
+                for(int j = i+1; j < array.length; j++){
+                    if(array[j].equals(array[i])){
+                        numberOfDuplicates++;
+                        beenChecked[j] = true;
+                    }
+                }
+                if(numberOfDuplicates >= maxNumberOfDuplications){
+                    elementsRemoved += numberOfDuplicates;
+                    for(int j = i; j < array.length; j++){
+                        if(array[j].equals(array[i])){
+                            dontCopy[j] = true;
+                        }
+                    }
+                }
+            }
+        }
+
+//        Integer[] newArr = new Integer[array.length - elementsRemoved];
+//        Integer index = 0;
+//        for(int i = 0; i < array.length; i++){
+//            if(!dontCopy[i]){
+//                newArr[index] = array[i];
+//                index++;
+//            }
+//        }
+//
+//        return newArr;
+        return remover(elementsRemoved, dontCopy);
+    }
+
+    @Override
+    public Integer[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        boolean[] dontCopy = new boolean[array.length];
+        boolean[] beenChecked = new boolean[array.length];
+        Integer elementsRemoved = 0;
+
+        for(int i = 0; i < array.length; i++){
+            if(!beenChecked[i]){
+                Integer numberOfDuplicates = 1;
+                for(int j = i+1; j < array.length; j++){
+                    if(array[j].equals(array[i])){
+                        numberOfDuplicates++;
+                        beenChecked[j] = true;
+                    }
+                }
+                if(numberOfDuplicates == exactNumberOfDuplications){
+                    elementsRemoved += numberOfDuplicates;
+                    for(int j = i; j < array.length; j++){
+                        if(array[j].equals(array[i])){
+                            dontCopy[j] = true;
+                        }
+                    }
+                }
+            }
+        }
+
+//        Integer[] newArr = new Integer[array.length - elementsRemoved];
+//        Integer index = 0;
+//        for(int i = 0; i < array.length; i++){
+//            if(!dontCopy[i]){
+//                newArr[index] = array[i];
+//                index++;
+//            }
+//        }
+//
+//        return newArr;
+        return remover(elementsRemoved, dontCopy);
+    }
+
+    protected Integer[] remover(Integer elementsRemoved, boolean[] dontCopy){
+        Integer[] newArr = new Integer[array.length - elementsRemoved];
+        Integer index = 0;
+        for(int i = 0; i < array.length; i++){
+            if(!dontCopy[i]){
+                newArr[index] = array[i];
+                index++;
+            }
+        }
+        return newArr;
+    }
 }
